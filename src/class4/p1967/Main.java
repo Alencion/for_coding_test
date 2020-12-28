50d0692293ae56ee7db4a222dd839f7ce16b17e1
JJL <rhqnrdl625@naver.com>
2020-12-28 22:48:26 +0900

    백준 class 4 1932, 1967, 1991, 2096

---
+++ src/class4/p1967/Main.java
@@ -0,0 +1,92 @@
+package class4.p1967;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<List<Edge>> adjList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            String[] split = br.readLine().split(" ");
+
+            int parent = Integer.parseInt(split[0]) - 1;
+            int child = Integer.parseInt(split[1]) - 1;
+            int weight = Integer.parseInt(split[2]);
+
+            adjList.get(parent).add(new Edge(child, weight));
+            adjList.get(child).add(new Edge(parent, weight));
+        }
+
+        int[] distV1 = dijkstra(0, n, adjList);
+
+        int maxIndex = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < distV1.length; i++) {
+            if (max < distV1[i]) {
+                maxIndex = i;
+                max = distV1[i];
+            }
+        }
+
+        int[] distV2 = dijkstra(maxIndex, n, adjList);
+        max = Integer.MIN_VALUE;
+        for (int i = 0; i < distV2.length; i++) {
+            max = Math.max(max, distV2[i]);
+        }
+        System.out.println(max);
+    }
+
+    private static int[] dijkstra(int start, int n, List<List<Edge>> adjList) {
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        boolean[] visited = new boolean[n];
+
+        queue.add(new Edge(start, 0));
+        while (!queue.isEmpty()) {
+            Edge poll = queue.poll();
+
+            visited[poll.to] = true;
+
+            for (Edge edge : adjList.get(poll.to)) {
+                if (!visited[edge.to] && dist[edge.to] > poll.weight + edge.weight) {
+                    dist[edge.to] = poll.weight + edge.weight;
+                    queue.add(new Edge(edge.to, dist[edge.to]));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int to;
+        int weight;
+
+        public Edge(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(weight, o.weight);
+        }
+    }
+}
---
+++ src/class4/p1991/Main.java
@@ -0,0 +1,76 @@
+package class4.p1991;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Map<Character, Node> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] split = br.readLine().split(" ");
+
+            char parent = split[0].charAt(0);
+            char left = split[1].charAt(0);
+            char right = split[2].charAt(0);
+
+            if (!map.containsKey(parent)) {
+                map.put(parent, new Node(parent));
+            }
+            Node parentNode = map.get(parent);
+
+            if (left != '.'){
+                map.put(left, new Node(left));
+                parentNode.left = map.get(left);
+            }
+
+            if (right != '.'){
+                map.put(right, new Node(right));
+                parentNode.right = map.get(right);
+            }
+        }
+
+        preOrder(map.get('A'));
+        System.out.println();
+        inOrder(map.get('A'));
+        System.out.println();
+        postOrder(map.get('A'));
+    }
+
+    private static void postOrder(Node a) {
+        if (a == null) return;
+        postOrder(a.left);
+        postOrder(a.right);
+        System.out.print(a.value);
+    }
+
+    private static void inOrder(Node a) {
+        if (a == null) return;
+        inOrder(a.left);
+        System.out.print(a.value);
+        inOrder(a.right);
+    }
+
+    private static void preOrder(Node a) {
+        if (a == null) return;
+        System.out.print(a.value);
+        preOrder(a.left);
+        preOrder(a.right);
+    }
+
+    static class Node{
+        char value;
+        Node left;
+        Node right;
+
+        public Node(char value) {
+            this.value = value;
+        }
+    }
+}
